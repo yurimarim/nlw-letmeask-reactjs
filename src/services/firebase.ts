477984fc0a373e60a9f6268e1041a621be573f11
yurimarim <yurimarim@hotmail.com>
2021-06-21 21:56:33 -0300

    Integração Firebase + License MIT

---
+++ src/services/firebase.ts
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+
+// firebase recomenda realizar a importação de cada serviço utilizado (firebase)
+import 'firebase/auth';
+import 'firebase/database';
+
+
+// Configuração de Variáveis Ambiente, .env.local => esse arquivo não sobe no github
+// no arquivo .env.local está todas as configurações da API, aqui estamos apenas referenciando elas.
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+// facilitar o acesso aos recursos, ao invés de sempre chamar as funções, 
+// atribuir as funções a uma const, no caso auth e database
+export const auth = firebase.auth();
+export const database = firebase.database();
