f78d49398db73517affd4c54d7215a9694120151
yurimarim <yurimarim@hotmail.com>
2021-06-24 18:53:29 -0300

    Integração com Realtime Database(Firebase) + criação/estilização da Room + Resumo aula 3 entre outras funcionalidades.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom'
-import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import { database } from '../services/firebase';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss'
 
@@ -14,7 +16,7 @@ import '../styles/auth.scss'
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
-
+  const [roomCode, setRoomCode] = useState('');
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle();
@@ -22,6 +24,23 @@ export function Home() {
 
 
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`rooms/${roomCode}`)
   }
 
 
@@ -42,10 +61,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
